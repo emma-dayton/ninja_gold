bc5125b488c377496c4ef4ed14930436bfd0001f
Emma Dayton <emma.dayton85@gmail.com>
2019-03-09 18:21:22 -0800

    a lot of changes to both server.py and index.html

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session
 from random import randint
+from datetime import datetime
 app = Flask(__name__)
 app.secret_key = 'a5f29b19f8ca2175b16f5f395ac8955c'
 # our index route will handle rendering our form
@@ -10,24 +11,45 @@ def index():
     if 'gold' not in session:
         session['gold'] = 0
     if 'count' not in session:
-        session['count'] = 0
+        session['count'] = 25
+    if 'log' not in session:
+        session['log'] = []
     return render_template("index.html")
 
 @app.route('/process_money', methods=['POST'])
 def process_money():
     print(request.form)
-    if request.form['building'] == 'farm':
-        session['gold'] += randint(8,13)
-    elif request.form['building'] == 'cave':
-        session['gold'] += randint(0,30)
-        session['count'] += 3
-    elif request.form['building'] == 'house':
-        session['gold'] += randint(3,15)
-    elif request.form['building'] == 'casino':
-        session['gold'] += randint(-50, 50)
-    session['count'] += 1
-    print(session['count'])
+    if session['count'] > 0:
+        if request.form['building'] == 'farm':
+            gold = randint(8,13)
+            session['gold'] += gold
+        elif request.form['building'] == 'cave':
+            gold = randint(0,30)
+            session['gold'] += gold
+            session['count'] -= randint(1, 10)
+        elif request.form['building'] == 'house':
+            gold = randint(3,15)
+            session['gold'] += gold
+        elif request.form['building'] == 'casino':
+            gold = randint(-50, 50)
+            session['gold'] += gold
+        session['count'] -= 1
+        print(session['count'])
+        now = datetime.now()
+        time = now.strftime("%B %-d, %Y %-I:%M:%S %p")
+        if gold >= 0:
+            activity = 'Earned ' + str(gold) + " pieces of gold at the " + request.form['building'] + f" ({time})"
+        elif gold < 0:
+            activity = 'Lost ' + str(gold * -1) + " pieces of gold at the " + request.form['building'] + f" ({time})"
+        session['log'].append(activity)
+    for x in session['log']:
+        print(x)
     return redirect('/')
+
+@app.route('/endgame', methods=['POST'])
+def endgame():
+    message = "Your final Gold tally: " + str(session['gold'])
+    return render_template("endgame.html")
 
 
 
